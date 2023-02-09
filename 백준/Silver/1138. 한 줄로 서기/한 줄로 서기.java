3f05ce3d51dc0ae4786fe2f687d5b1bb66a7dba6
승연 <47537803+O-Wensu@users.noreply.github.com>
2023-02-09 17:11:03 +0900

    [Silver II] Title: 한 줄로 서기, Time: 164 ms, Memory: 16508 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(br.readLine());
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        List<Integer> arr = new ArrayList<>();
+        int[] line = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            int temp = Integer.parseInt(token.nextToken());
+            arr.add(temp);
+        }
+
+        for (int i = 0; i < size; i++) {
+            int count = -1;
+            int index = findIndex(line, 0);
+            int pos = 0;
+            for (int j = index; j < size; j++) {
+                if (line[j] == 0) count++;
+
+                if (count == arr.get(i)) {
+                    pos = j;
+                    break;
+                }
+            }
+            line[pos] = i + 1;
+        }
+        String str = Arrays.stream(line)
+                .mapToObj(String::valueOf)
+                .reduce((x,y) -> x + " " + y)
+                .get();
+        System.out.print(str);
+    }
+
+    public static int findIndex(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) return i;
+        }
+        return -1;
+    }
+}
