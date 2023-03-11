a99a82d939f64e9ba260a981e8830967e079ca37
승연 <47537803+O-Wensu@users.noreply.github.com>
2023-03-12 01:11:15 +0900

    [Silver III] Title: 피보나치 함수, Time: 144 ms, Memory: 16048 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,25 +1,15 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[][] dp = new int[41][2];
 
-    static int[] fibo(int x) {
-        if (dp[x][0] == Integer.MIN_VALUE || dp[x][1] == Integer.MIN_VALUE) {
-            dp[x][0] = fibo(x-1)[0] + fibo(x-2)[0];
-            dp[x][1] = fibo(x-1)[1] + fibo(x-2)[1];
-        }
-        return dp[x];
-    }
+    public static int[][] dp = new int[41][2];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int size = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < 41; i++) {
-            dp[i][0] = Integer.MIN_VALUE;
-            dp[i][1] = Integer.MIN_VALUE;
-        }
+        int size = Integer.parseInt(br.readLine());
 
         dp[0][0] = 1;
         dp[0][1] = 0;
@@ -28,8 +18,11 @@ public class Main {
 
         for (int i = 0; i < size; i++) {
             int x = Integer.parseInt(br.readLine());
-            int[] result = fibo(x);
-            bw.write(result[0] + " " + result[1] + "\n");
+            for (int j = 2; j < x + 1; j++) {
+                dp[j][0] = dp[j - 1][0] + dp[j - 2][0];
+                dp[j][1] = dp[j - 1][1] + dp[j - 2][1];
+            }
+            bw.write(dp[x][0] + " " + dp[x][1] + "\n");
         }
         bw.flush();
         bw.close();
