93f40fe46679f44e6ecebbc988f0d03dc4385c03
승연 <47537803+O-Wensu@users.noreply.github.com>
2023-03-14 16:21:18 +0900

    [Silver V] Title: 다리 놓기, Time: 152 ms, Memory: 14812 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,34 +1,32 @@
 import java.io.*;
 import java.util.*;
 
-//제출 안했음
 public class Main {
-    static int[][] dp = new int[30][30];
+    public static int[][] dp = new int[30][30];
+
+    public static int combination(int m, int n) {
+        if (dp[m][n] > 0) {
+            return dp[m][n];
+        }
+        
+        if (n == 0 || m == n) {
+            return 1;
+        }
+        
+        return dp[m][n] = combination(m - 1, n - 1) + combination(m - 1, n);
+    }
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int size = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp.length; j++) {
-                if (i == j || j == 0) dp[i][j] = 1;
-            }
-        }
-
         for (int i = 0; i < size; i++) {
             StringTokenizer tk = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(tk.nextToken());
             int m = Integer.parseInt(tk.nextToken());
 
-
-            for (int j = 1; j <= m; j++) {
-                for (int k = 1; k <= n; k++) {
-                    dp[j][k] = dp[j-1][k-1] + dp[j-1][k];
-                }
-            }
-            bw.write(dp[m][n] + "\n");
+            int result = combination(m, n);
+            bw.write(result + "\n");
         }
         bw.flush();
         bw.close();
