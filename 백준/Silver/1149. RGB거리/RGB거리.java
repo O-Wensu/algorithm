b9327ffb4d9caf3d73160cb03d3b79da5792b4c2
승연 <47537803+O-Wensu@users.noreply.github.com>
2023-03-15 16:40:07 +0900

    [Silver I] Title: RGB거리, Time: 136 ms, Memory: 14560 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,33 +1,16 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    // 0: Red 1: Green 2: Blue
     static int[][] cost;
     static int[][] dp;
 
-    static int paint(int x, int color) {
-        if (dp[x][color] > 0) {
-            return dp[x][color];
-        }
-
-        if (color == 0) { //Red
-            dp[x][0] = Math.min(paint(x-1,1), paint(x-1,2)) + cost[x][0];
-        } else if (color == 1) { //Green
-            dp[x][1] = Math.min(paint(x-1,0), paint(x-1,2)) + cost[x][1];
-        } else { //Blue
-            dp[x][2] = Math.min(paint(x-1,0), paint(x-1,1)) + cost[x][2];
-        }
-        return dp[x][color];
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int size = Integer.parseInt(br.readLine());
-        cost = new int[size][3]; //각 집 색상 cost
-        dp = new int[size][3]; //최소 비용
+        cost = new int[size][3];
+        dp = new int[size][3];
 
         for (int i = 0; i < size; i++) {
             StringTokenizer tk = new StringTokenizer(br.readLine());
@@ -40,11 +23,12 @@ public class Main {
         dp[0][1] = cost[0][1];
         dp[0][2] = cost[0][2];
 
-        int red = paint(size - 1, 0);
-        int green = paint(size - 1, 1);
-        int blue = paint(size - 1, 2);
-        int minCost = Math.min(red, Math.min(green, blue));
-
+        for (int i = 1; i < size; i++) {
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + cost[i][0];
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + cost[i][1];
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + cost[i][2];
+        }
+        int minCost = Math.min(dp[size-1][0], Math.min(dp[size-1][1], dp[size-1][2]));
         System.out.println(minCost);
     }
 }
