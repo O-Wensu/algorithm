3064ec414d1d7621057982e85721e82db108ba4b
승연 <47537803+O-Wensu@users.noreply.github.com>
2023-06-23 21:33:36 +0900

    [Bronze I] Title: 팰린드롬수, Time: 120 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while(true) {
+            String original = br.readLine();
+            if(original.equals("0")) break;
+
+            String reverse = new StringBuilder(original).reverse().toString();
+            if (original.equals(reverse)) {
+                bw.write("yes\n");
+            } else {
+                bw.write("no\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
