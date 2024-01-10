4decfd520a993a7e90c56b7f4d673d78ff73e709
승연 <47537803+O-Wensu@users.noreply.github.com>
2024-01-11 00:11:30 +0900

    [Bronze I] Title: 단어 공부, Time: 292 ms, Memory: 22032 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,31 +1,27 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine().toLowerCase();
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : str.toCharArray()) {
-            int num = 1;
-            if (map.containsKey(c)) num = map.get(c) + 1;
-            map.put(c, num);
-        }
-
-        String key = "";
+        String word = br.readLine();
+        int[] count = new int[26];
         int max = Integer.MIN_VALUE;
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > max) {
-                max = entry.getValue();
-                key = entry.getKey().toString().toUpperCase();
+        char answer = ' ';
+        for (char c: word.toCharArray()) {
+            char upperChar = Character.toUpperCase(c);
+            int index = upperChar - 'A';
+            count[index] += 1;
+            if (max < count[index]) {
+                max = count[index];
+                answer = upperChar;
             }
         }
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == max && !key.equals(entry.getKey().toString().toUpperCase())) {
-                key = "?";
-                break;
+        for (int i = 0; i < count.length; i++) {
+            if (i == answer - 'A') continue;
+            if (max == count[i]) {
+                answer = '?';
             }
         }
-        System.out.println(key);
+        System.out.println(answer);
     }
 }
