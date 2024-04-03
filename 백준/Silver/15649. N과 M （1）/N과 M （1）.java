55a759642dd3f7fabd49e4976a647847edc3a06a
승연 <47537803+O-Wensu@users.noreply.github.com>
2024-04-03 22:26:13 +0900

    [Silver III] Title: N과 M (1), Time: 264 ms, Memory: 22276 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,40 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] selected, used;
 
-    public static int n, m;
-    public static boolean[] visited;
-    public static int[] arr;
-    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static void dfs(int index) throws IOException {
-        if (index == m) {
-            for (int i : arr) {
-                bw.write(i + " ");
-            }
-            bw.write("\n");
-            return;
-        }
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(tk.nextToken());
+        M = Integer.parseInt(tk.nextToken());
+        selected = new int[M + 1];
+        used = new int[N + 1];
+    }
 
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                arr[index] = i;
-                dfs(index + 1);
-                visited[i] = false;
+    static void rec_func(int k) {
+        if (k == M + 1) {
+            for (int i = 1; i <= M; i++) sb.append(selected[i]).append(' ');
+            sb.append("\n");
+        } else {
+            for (int cand = 1; cand <= N; cand++) {
+                if (used[cand] == 1) continue;
+                selected[k] = cand; used[cand] = 1;
+                rec_func(k + 1);
+                selected[k] = 0; used[cand] = 0;
             }
         }
     }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tk = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(tk.nextToken());
-        m = Integer.parseInt(tk.nextToken());
-        visited = new boolean[n + 1];
-        arr = new int[m];
 
-        dfs(0);
-        bw.flush();
-        bw.close();
-        br.close();
+    public static void main(String[] args) throws IOException {
+        input();
+        rec_func(1);
+        System.out.println(sb.toString());
     }
 }
