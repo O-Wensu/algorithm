ef870a150841266e61ea51427881ea618342173a
승연 <47537803+O-Wensu@users.noreply.github.com>
2024-04-08 22:25:59 +0900

    [Silver IV] Title: 수 찾기, Time: 592 ms, Memory: 44168 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,49 +1,50 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int sizeA = Integer.parseInt(br.readLine());
-        int[] a = new int[sizeA];
-        StringTokenizer tk = new StringTokenizer(br.readLine());
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tk;
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] A;
 
-        for (int i = 0; i < sizeA; i++) {
-            a[i] = Integer.parseInt(tk.nextToken());
-        }
-
-        int sizeB = Integer.parseInt(br.readLine());
-        int[] b = new int[sizeB];
+    static void input() throws IOException {
+        N = Integer.parseInt(br.readLine());
         tk = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i <sizeB; i++) {
-            b[i] = Integer.parseInt(tk.nextToken());
+        A = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(tk.nextToken());
         }
+        M = Integer.parseInt(br.readLine());
+        tk = new StringTokenizer(br.readLine());
+    }
 
-        Arrays.sort(a);
-        
-        for (int i = 0; i < sizeB; i++) {
-            int target = b[i];
-            int start = 0;
-            int end = sizeA - 1;
-            int result = 0;
-            while(start <= end) {
-                int mid = (start + end) / 2;
-                if (target == a[mid]) {
-                    result = 1;
-                    break;
-                }
-                if (target < a[mid]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            }
-            bw.write(result + "\n");
+    static boolean binary_search(int[] arr, int l, int r, int x) {
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (arr[mid] == x) return true;
+
+            if (arr[mid] < x)
+                l = mid + 1;
+            else
+                r = mid - 1;
         }
-        bw.flush();
-        bw.close();
-        br.close();
+        return false;
+    }
+
+    static void pro() {
+        Arrays.sort(A, 1, N + 1);
+
+        for (int i = 1; i <= M; i++) {
+            int x = Integer.parseInt(tk.nextToken());
+            if (binary_search(A, 1, N, x)) sb.append(1).append('\n');
+            else sb.append(0).append('\n');
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+        System.out.print(sb);
     }
 }
