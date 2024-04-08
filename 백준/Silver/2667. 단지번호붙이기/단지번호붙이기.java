c74a4ce93ef89d0b5c2e98ef23d65642779217ee
승연 <47537803+O-Wensu@users.noreply.github.com>
2024-04-09 04:51:49 +0900

    [Silver I] Title: 단지번호붙이기, Time: 128 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,59 +1,57 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N, group_cnt;
+    static int[][] dir = {{-1,0}, {1, 0}, {0, -1}, {0, 1}};
+    static int[][] adj;
+    static boolean[][] visit;
+    static ArrayList<Integer> group = new ArrayList<>();
 
-    public static int n;
-    public static int[][] graph;
-    public static int[] dx = {-1, 1, 0, 0};
-    public static int[] dy = {0, 0, -1, 1};
-    public static int count = 0;
-
-    public static void dfs(int x, int y) {
-        if (x <= -1 || x >= n || y <= -1 || y >= n) {
-            return;
-        }
-        if (graph[x][y] == 1) {
-            graph[x][y] = 0;
-
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                dfs(nx, ny);
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        adj = new int[N][N];
+        visit = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            char[] chars = br.readLine().toCharArray();
+            for (int j = 0; j < N; j++) {
+                adj[i][j] = chars[j] - '0';
             }
-            count++;
         }
     }
 
+    static void dfs(int x, int y) {
+        group_cnt++;
+        visit[x][y] = true;
+
+        for (int i = 0; i < dir.length; i++) {
+            int nx = x + dir[i][0];
+            int ny = y + dir[i][1];
+            if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+            if (adj[nx][ny] == 0 || visit[nx][ny]) continue;
+            dfs(nx, ny);
+        }
+    }
+
+    static void pro() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (adj[i][j] == 0 || visit[i][j]) continue;
+                group_cnt = 0;
+                dfs(i, j);
+                group.add(group_cnt);
+            }
+        }
+        Collections.sort(group);
+        sb.append(group.size()).append('\n');
+        for (int i : group) sb.append(i).append('\n');
+    }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        ArrayList<Integer> results = new ArrayList<>();
-        n = Integer.parseInt(br.readLine());
-        graph = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            char[] chars = br.readLine().toCharArray();
-            for (int j = 0; j < n; j++) {
-                graph[i][j] = chars[j] - '0';
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                dfs(i,j);
-                if (count != 0) {
-                    results.add(count);
-                    count = 0;
-                }
-            }
-        }
-        Collections.sort(results);
-        bw.write(results.size() + "\n");
-        for (Integer result : results) {
-            bw.write(result + "\n");
-        }
-        bw.flush();
-        bw.close();
+        input();
+        pro();
+        System.out.println(sb);
     }
 }
