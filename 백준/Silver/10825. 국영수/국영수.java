03b6b59562c261b01116e0cad72479154132709f
승연 <47537803+O-Wensu@users.noreply.github.com>
2024-04-08 10:01:37 +0900

    [Silver IV] Title: 국영수, Time: 724 ms, Memory: 59376 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.java
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -1,75 +1,50 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static class Student {
-        String name;
-        int korean;
-        int english;
-        int math;
+    static class Student implements Comparable<Student> {
+        public String name;
+        public int kor, eng, math;
 
-        public Student(String name, int korean, int english, int math) {
-            this.name = name;
-            this.korean = korean;
-            this.english = english;
-            this.math = math;
+        @Override
+        public int compareTo(Student other) {
+            if (kor != other.kor) return other.kor - kor;
+            if (eng != other.eng) return eng - other.eng;
+            if (math != other.math) return other.math - math;
+            return name.compareTo(other.name);
         }
+    }
 
-        public String getName() {
-            return name;
+    static int N;
+    static StringBuilder sb = new StringBuilder();
+    static Student[] students;
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        students = new Student[N];
+        StringTokenizer tk;
+        for (int i = 0; i < N; i++) {
+            tk = new StringTokenizer(br.readLine());
+            students[i] = new Student();
+            students[i].name = tk.nextToken();
+            students[i].kor = Integer.parseInt(tk.nextToken());
+            students[i].eng = Integer.parseInt(tk.nextToken());
+            students[i].math = Integer.parseInt(tk.nextToken());
         }
+    }
 
-        public int getKorean() {
-            return korean;
-        }
+    static void pro() {
+        Arrays.sort(students);
 
-        public int getEnglish() {
-            return english;
-        }
-
-        public int getMath() {
-            return math;
+        for (int i = 0; i < N; i++) {
+            sb.append(students[i].name).append("\n");
         }
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        Student[] students = new Student[n];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer tk = new StringTokenizer(br.readLine());
-            String name = tk.nextToken();
-            int ko = Integer.parseInt(tk.nextToken());
-            int en = Integer.parseInt(tk.nextToken());
-            int math = Integer.parseInt(tk.nextToken());
-
-            students[i] = new Student(name, ko, en, math);
-        }
-
-        Comparator<Student> comparator = new Comparator<Student>() {
-            @Override
-            public int compare(Student o1, Student o2) {
-                if(o1.korean < o2.korean) {
-                    return 1;
-                }else if(o1.korean == o2.korean) {
-                    if(o1.english > o2.english) {
-                        return 1;
-                    }else if(o1.english == o2.english) {
-                        if(o1.math < o2.math) {
-                            return 1;
-                        }else if (o1.math == o2.math) {
-                            return o1.name.compareTo(o2.name);
-                        }
-                    }
-                }
-                return -1;
-            }
-        };
-
-        Arrays.sort(students, comparator);
-
-        for (Student student : students) {
-            System.out.println(student.name);
-        }
+        input();
+        pro();
+        System.out.println(sb.toString());
     }
 }
