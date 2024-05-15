a76027754cd89cc7f21e801f8402be990d3f6fb6
승연 <47537803+O-Wensu@users.noreply.github.com>
2024-05-15 23:28:23 +0900

    [Silver I] Title: 숨바꼭질, Time: 168 ms, Memory: 18712 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,46 +1,58 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    static final int MAX = 100000;
+    static int N, K;
+    static boolean[] visit;
+    static int[] dist;
 
-    static int n, k;
-    static int[] visited = new int[100001];
-    static int[] dx = {-1, 1};
+     static void input() throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         StringTokenizer tk = new StringTokenizer(br.readLine());
+         N = Integer.parseInt(tk.nextToken());
+         K = Integer.parseInt(tk.nextToken());
+         visit = new boolean[MAX + 1];
+         dist = new int[MAX + 1];
+     }
 
-    static void bfs(int x) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(x);
-        visited[x] = 1;
+     static void bfs() {
+         Queue<Integer> Q = new LinkedList<>();
+         Q.offer(N);
+         visit[N] = true;
 
-        while(!q.isEmpty()) {
-            x = q.poll();
-            for (int i = 0; i < 3; i++) {
-                int nx;
-                if (i == 2) {
-                    nx = 2 * x;
-                } else {
-                    nx = x + dx[i];
-                }
-                if (nx == k) {
-                    System.out.println(visited[x]);
-                    return;
-                }
-                if (nx < 0 || nx >= visited.length) continue;
-                if (visited[nx] == 0) {
-                    visited[nx] = visited[x] + 1;
-                    q.offer(nx);
-                }
-            }
-        }
-    }
+         while(!Q.isEmpty()) {
+             int x = Q.poll();
+             int y = x - 1;
+             if (y >= 0 && y <= MAX && !visit[y]){
+                 visit[y] = true;
+                 dist[y] = dist[x] + 1;
+                 Q.add(y);
+             }
+
+             y = x + 1;
+             if (y >= 0 && y <= MAX && !visit[y]){
+                 visit[y] = true;
+                 dist[y] = dist[x] + 1;
+                 Q.add(y);
+             }
+
+             y = x * 2;
+             if (y >= 0 && y <= MAX && !visit[y]){
+                 visit[y] = true;
+                 dist[y] = dist[x] + 1;
+                 Q.add(y);
+             }
+         }
+     }
+
+     static void pro() {
+         bfs();
+         System.out.println(dist[K]);
+     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tk = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(tk.nextToken());
-        k = Integer.parseInt(tk.nextToken());
-
-        if (n == k) System.out.println("0");
-        else bfs(n);
+        input();
+        pro();
     }
 }
